f48819a61835f49ddfc25f0edf19a40ecf21c9b3
Harshit Joshi <joshiharshit291@gmail.com>
2026-04-30 17:39:58 +0530

    packages & server setup

---
+++ dashboard-backend/server.js
@@ -0,0 +1,15 @@
+import 'dotenv/config';
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+
+app.use(cors());
+app.use(express.json());
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
